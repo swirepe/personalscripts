53909ac329e3367e83de986db9420e078c1647ea
Peter Swire <pswire@cargometrics.com>
2012-08-31 18:32:43 -0400

    can now cycle through statuses in order

--- tmux/tmux_switch_status.py
+++ tmux/tmux_switch_status.py
@@ -1,17 +1,37 @@
 #!/usr/bin/python
 
 import os
-from random import choice
+import shelve
 
-# get the the right directory
+# get the the right directory, load them in order
 os.chdir( os.path.join(os.environ["SCRIPTS_DIR"], "tmux")) 
+
+# use a shelf, because environments are weird here
+localenv = shelve.open(".current_tmux_status_index")
+
+try:
+    index = localenv["TMUX_CURRENT_STATUS"]
+except KeyError:
+    index = 0
+
 
 statuses = os.listdir("./status")
 
-newstat = choice(statuses)
 
+if index >= len(statuses):
+    index = 0
+    
+    
+newstat = statuses[index]
+
+# write the contents to the file that'll be sourced by tmux.conf
 instat = open("./status/" + newstat, 'r').readlines()
 outstat = open("./current.tmux_status", 'w')
 outstat.write("".join(instat))
 
 outstat.close()
+
+
+# update the environment
+localenv["TMUX_CURRENT_STATUS"] = index + 1
+localenv.close()
