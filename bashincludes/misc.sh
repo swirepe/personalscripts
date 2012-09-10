384b43d1431a519eb8a9a19dc10289ba031847ca
Peter Swire <pswire@cargometrics.com>
2012-09-10 15:40:34 -0400

    xpipe = run through different commands, trying them out.

--- bashincludes/misc.sh
+++ bashincludes/misc.sh
@@ -7,11 +7,50 @@ function lastrun {
 # http://xion.org.pl/2012/05/10/dreampie-with-virtualenv/
 alias dream='(dreampie $(which python) &>/dev/null &)'
 
+
+function ingitrepo {
+    # returns zero if in a git repo, nonzero if not
+    local  __resultvar=$1
+    git rev-parse 2> /dev/null > /dev/null
+    local myresult=$?
+    if [[ "$__resultvar" ]]; then
+        eval $__resultvar="$myresult"
+    else
+        echo "$myresult"
+    fi
+}
+
+
 function findinfiles {
+   # we use git --ls-files so that we take into account the .gitignores
+   local _ingit=$(ingitrepo)
+   if [ "$_ingit" -eq "0" ] ;
+   then
+       findinfiles_git $@
+   else
+       findinfiles_regular $@        
+   fi
+}
+
+
+function findinfiles_regular {
+   local searchString 
    for searchString in "$@"
    do
        echo "Files containing $searchString"
        echo "--------"
        find . | xargs grep "$searchString" -sl
+   done
+}
+
+
+function findinfiles_git {
+   local searchString
+   git branch
+   for searchString in "$@"
+   do
+       echo "Files containing $searchString"
+       echo "--------"
+       git ls-files | xargs grep "$searchString" -sl | xpipe "git status -s" "echo"
    done
 }
