6d9d0ca8e90094531d24f6d90af6161e45f78c88
Peter Swire <pswire@cargometrics.com>
2012-09-26 15:58:48 -0400

    take 3: a repl

--- alwaysontop/alwaysontop.sh
+++ alwaysontop/alwaysontop.sh
@@ -3,42 +3,34 @@ function alwaysontop {
     if [ "$ALWAYSONTOP" != "TRUE" ]
     then
         export ALWAYSONTOP="TRUE"
-        export OLD_PROMPT_COMMAND="$PROMPT_COMMAND"
-        if [ "$PROMPT_COMMAND" ]
-        then
-            PROMPT_COMMAND="$PRMOPT_COMMAND ; tput cup 0 0 ; tput el ; tput el1 "
-        else
-            PROMPT_COMMAND="tput cup 0 0 ; tput el ; tput el1 "
-        fi
+        _toprepl
     fi
-    
-    bind 'RETURN: "\e[1~clearallbuttop && \e[4~\n"'
 }
+
+
+function _toprepl {
+    local _usrcmd
+    clear
+    while [ "$ALWAYSONTOP" == "TRUE" ]
+    do
+        echo -en "$PS1"
+        read -p "" _usrcmd
+        clear
+        eval _usrcmd
+        tput cup 0 0
+        tput el
+        tput el1
+    done
+}
+
 
 
 function unalwaysontop {
     if [ "$ALWAYSONTOP" == "TRUE"  ]
     then
-        if [ -n $OLD_PROMPT_COMMAND ]
-        then
-            PROMPT_COMMAND="$OLD_PRMOPT_COMMAND"
-            ALWAYSONTOP="FALSE"
-        fi
+        ALWAYSONTOP="FALSE"
     fi
-   bind 'RETURN: "\n'
+   
 }
-
-function clearallbuttop {
-    for (( i=1; i <= $LINES; i++ ))
-    do
-        tput cup $i 0
-        tput el
-        tput el1 
-    done
-    tput cup 0 0 
-}
-
-export clearallbuttop
-
 
 
