5b26661ef2bf4dc60e8f6f8f653b33ee9ef1dcdc
Peter Swire <pswire@cargometrics.com>
2012-09-26 16:07:21 -0400

    doesn't clear after each command, but does enough of what i want it to

--- alwaysontop/alwaysontop.sh
+++ alwaysontop/alwaysontop.sh
@@ -3,34 +3,30 @@ function alwaysontop {
     if [ "$ALWAYSONTOP" != "TRUE" ]
     then
         export ALWAYSONTOP="TRUE"
-        _toprepl
+        export OLD_PROMPT_COMMAND="$PROMPT_COMMAND"
+        if [ "$PROMPT_COMMAND" ]
+        then
+            PROMPT_COMMAND="$PRMOPT_COMMAND ; tput cup 0 0 ; tput el ; tput el1 "
+        else
+            PROMPT_COMMAND="tput cup 0 0 ; tput el ; tput el1 "
+        fi
     fi
 }
 
-
-function _toprepl {
-    local _usrcmd
-    clear
-    while [ "$ALWAYSONTOP" == "TRUE" ]
-    do
-        echo -en "$PS1"
-        read -p "" _usrcmd
-        clear
-        eval _usrcmd
-        tput cup 0 0
-        tput el
-        tput el1
-    done
-}
 
 
 
 function unalwaysontop {
     if [ "$ALWAYSONTOP" == "TRUE"  ]
     then
-        ALWAYSONTOP="FALSE"
+        if [ -n $OLD_PROMPT_COMMAND ]
+        then
+            PROMPT_COMMAND="$OLD_PRMOPT_COMMAND"
+            ALWAYSONTOP="FALSE"
+        fi
     fi
    
 }
+
 
 
