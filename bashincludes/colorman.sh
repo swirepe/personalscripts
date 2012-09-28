750c393d01d5ca69b085cebb51684ed229e8adb8
Peter Swire <pswire@cargometrics.com>
2012-09-28 10:40:04 -0400

    added colors to man pages

---
+++ bashincludes/colorman.sh
@@ -0,0 +1,13 @@
+# for colorful man pages
+# http://christopherolah.wordpress.com/tag/command-line/
+#note that the variables could easily be other
+#escape sequences.
+# For colourful man pages (CLUG-Wiki style)
+export LESS_TERMCAP_mb=$'\E[01;31m'
+export LESS_TERMCAP_md=$'\E[01;31m'
+export LESS_TERMCAP_me=$'\E[0m'
+export LESS_TERMCAP_se=$'\E[0m'
+export LESS_TERMCAP_so=$'\E[01;44;33m'
+export LESS_TERMCAP_ue=$'\E[0m'
+export LESS_TERMCAP_us=$'\E[01;32m'
+
