256da5cefa719810f1c8ae6ba6d96382a02eb733
Peter Swire <pswire@cargometrics.com>
2012-10-01 11:52:12 -0400

    changed man colors to something more pleasant

--- bashincludes/colorman.sh
+++ bashincludes/colorman.sh
@@ -1,13 +1,11 @@
-# for colorful man pages
-# http://christopherolah.wordpress.com/tag/command-line/
-#note that the variables could easily be other
-#escape sequences.
-# For colourful man pages (CLUG-Wiki style)
-export LESS_TERMCAP_mb=$'\E[01;31m'
-export LESS_TERMCAP_md=$'\E[01;31m'
-export LESS_TERMCAP_me=$'\E[0m'
-export LESS_TERMCAP_se=$'\E[0m'
-export LESS_TERMCAP_so=$'\E[01;44;33m'
-export LESS_TERMCAP_ue=$'\E[0m'
-export LESS_TERMCAP_us=$'\E[01;32m'
+# Less Colors for Man Pages
+export LESS_TERMCAP_mb=$'\E[01;31m'       # begin blinking
+export LESS_TERMCAP_md=$'\E[01;38;5;74m'  # begin bold
+export LESS_TERMCAP_me=$'\E[0m'           # end mode
+export LESS_TERMCAP_se=$'\E[0m'           # end standout-mode
+export LESS_TERMCAP_so=$'\E[38;5;246m'    # begin standout-mode - info box
+export LESS_TERMCAP_ue=$'\E[0m'           # end underline
+export LESS_TERMCAP_us=$'\E[04;38;5;146m' # begin underline
 
+
+# http://linuxtidbits.wordpress.com/2009/03/23/less-colors-for-man-pages/
