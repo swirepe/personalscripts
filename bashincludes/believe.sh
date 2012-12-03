f6ef83ded9502b0e10b2f065b59abafd533063b3
Peter Swire <swirepe@gmail.com>
2012-12-02 23:31:17 -0500

    now fortune pulls from my own private stock, and word wraps on soft points

--- bashincludes/believe.sh
+++ bashincludes/believe.sh
@@ -1,4 +1,5 @@
 echo -en $COLOR_White
 (probexit 0.1 && echo "Believe in yourself.") ||
-    (probexit 0.3 && fortune -s)
+    (probexit 0.3 && fortune -s | fold -s) ||
+    (probexit 0.3 && fortune /home/swirepe/pers/quotes/compiled | fold -s)
 echo -en $COLOR_off
