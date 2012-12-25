a481c3d8d95ec5cd865d22fd3cac0fb1b4f7975a
Peter Swire <swirepe@gmail.com>
2012-12-24 23:10:42 -0500

    prettied up the script for connecting to my mom's wifi (wep 64 bit), as well as added a check to see if the connection worked.

--- homewifi.sh
+++ homewifi.sh
@@ -1,9 +1,23 @@
 # The wifi at home uses wep 64 bit.
 # the ubuntu gui doesn't support that
+# http://superuser.com/questions/120402/wep-authentication-suddenly-failing-in-ubuntu
 
+echo -e "${COLOR_White}[home-wifi] Stopping the network manager.${COLOR_off}"
 sudo /etc/init.d/network-manager stop
-# sudo iwlist wlan0 scan
+# sudo iwlist wlan0 scan  ## this command doesn't work
+
+echo -e "${COLOR_White}[home-wifi] Setting the essid.${COLOR_off}"
 sudo iwconfig wlan0 essid V8XB1
+
+echo -e "${COLOR_White}[home-wifi] Setting the key.${COLOR_off}"
 sudo iwconfig wlan0 key 12E5D82969 open
+
+echo -e "${COLOR_White}[home-wifi] Starting things back up.${COLOR_off}"
 sudo dhclient wlan0
 
+if [ "`ping -c 1 google.com`" ]
+then
+    echo -e "${COLOR_IWhite}[home-wifi] Online.${COLOR_off}"
+else
+    echo -e "${COLOR_IWhite}[home-wifi] Connection FAILED.${COLOR_off}"
+fi
