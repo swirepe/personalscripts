dc4abf3414188045d8e4c802416f4149740e641f
Peter Swire <swirepe@gmail.com>
2013-01-04 17:36:36 -0500

    now my fortunes are held in memory too

--- bashincludes/fortunes.sh
+++ bashincludes/fortunes.sh
@@ -1,5 +1,44 @@
+export FORTUNES_DIR="/usr/share/games/fortunes"
+export FORTUNES_PERS_DIR="/home/swirepe/pers/quotes/fortunes_pers"
+
+
+function load_fortunes {
+    # load the fortune files into memory
+    if [[  -d "/tmp/ramdisk/fortunes" ]]
+    then
+       FORTUNES_DIR="/tmp/ramdisk/fortunes"
+    else
+        # report this in purple
+       echo -e "\e[0;35m[fortunes.sh] Copying fortunes to memory.\e[0m"
+       FORTUNES_DIR=$(/home/swirepe/pers/scripts/toramdisk.sh "$FORTUNES_DIR")
+    fi
+    
+    if [[  -d "/tmp/ramdisk/fortunes_pers" ]]
+    then
+       FORTUNES_PERS_DIR="/tmp/ramdisk/fortunes_pers"
+    else
+        # report this in purple
+       echo -e "\e[0;35m[fortunes.sh] Copying personal fortunes to memory.\e[0m"
+       FORTUNES_PERS_DIR=$(/home/swirepe/pers/scripts/toramdisk.sh "$FORTUNES_PERS_DIR")
+    fi
+    
+}
+
+
+function unmount-fortunes {
+    sudo umount /tmp/ramdisk/fortunes
+    sudo umount /tmp/ramdisk/fortunes_pers    
+    sudo rm -r /tmp/ramdisk/fortunes
+    sudo rm -r /tmp/ramdisk/fortunes_pers   
+}
+
+
+
+## This part actually displays the fortunes
+load_fortunes
+
 echo -en $COLOR_White
 (probexit 0.1 && echo "Believe in yourself.") ||
-    (probexit 0.3 && fortune -s | fold -s) ||
-    (probexit 0.3 && fortune /home/swirepe/pers/quotes/compiled | fold -s)
+    (probexit 0.3 && fortune -s "$FORTUNES_DIR" | fold -s) ||
+    (probexit 0.3 && fortune "$FORTUNES_PERS_DIR" | fold -s)
 echo -en $COLOR_off
