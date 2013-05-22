dd81a9d83e053b60dea3c3ded70a794ec3695b4a
Peter Swire <swirepe@pswire-devserver.localdomain>
2013-05-22 16:50:01 -0400

    now when you connect via ssh, it shows your tmux sessions

---
+++ bashincludes/lstmux.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+function lstmux {
+	SESSIONS=$(tmux list-sessions 2> /dev/null)
+	if [[ "$SESSIONS" ]] 
+	then
+	     
+		echo -en "$COLOR_BGreen"
+		echo "Available Tmux Sessions: "
+		echo -en "$COLOR_Green"
+		
+		echo "$SESSIONS" | sed 's/^/    /'
+
+		echo -en "$COLOR_off"
+	fi
+}
+
+if [ "$SSH_CONNECTION" ]
+then
+	lstmux
+fi
