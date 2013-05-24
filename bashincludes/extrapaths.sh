d34ae106b79600d318966176b88a9e520b0fb562
Peter Swire <swirepe@pswire-devlaptop.dhcp.tripadvisor.com>
2013-05-24 16:16:08 -0400

    Squashed commit of the following:
    commit 023de04423d22049d14fcc43ee468ffcc3289948
    Author: Peter Swire <swirepe@pswire-devlaptop.dhcp.tripadvisor.com>
    Date:   Fri May 24 16:09:10 2013 -0400
    
        bashrc is now bashrc.init
    
    commit 36729d7b5d775099159da61e416c4da0e536cace
    Author: Peter Swire <swirepe@pswire-devlaptop.dhcp.tripadvisor.com>
    Date:   Fri May 24 16:07:44 2013 -0400
    
        bashrc.include is now bashrc_core in bashincludes
    
    commit 1a9f72f5e1e80d1bf7d4a619a50a9d0618c1be0a
    Author: Peter Swire <swirepe@pswire-devlaptop.dhcp.tripadvisor.com>
    Date:   Fri May 24 16:00:48 2013 -0400
    
        moved around some bashrc stuff

---
+++ bashincludes/extrapaths.sh
@@ -0,0 +1,10 @@
+
+# Matlab
+export PATH="$PATH:/home/swirepe/Matlab"
+export PATH="$PATH:/home/swirepe/Matlab/bin"
+    
+
+# ruby rvm
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
+PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
+
