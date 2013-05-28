4024683e5f784b8951efa1bbf5aff904bb65631c
Peter Swire <swirepe@pswire-devlaptop.dhcp.tripadvisor.com>
2013-05-28 14:06:42 -0400

    some safeguards added

--- bashincludes/errorinred.sh
+++ bashincludes/errorinred.sh
@@ -1,4 +1,9 @@
 
 # see https://github.com/sickill/stderred
-
-export LD_PRELOAD="$SCRIPTS_DIR/src/stderred/build/libstderred.so${LD_PRELOAD:+:$LD_PRELOAD}"
+if [[ -f "$SCRIPTS_DIR/src/stderred/build/libstderred.so" ]]
+then
+    export LD_PRELOAD="$SCRIPTS_DIR/src/stderred/build/libstderred.so${LD_PRELOAD:+:$LD_PRELOAD}"
+else
+    echo "[errorinred.sh] libstderred.so not found."
+fi
+    
