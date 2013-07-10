f0dfcb61162aee19eca007817be778e626795b92
Peter Swire <swirepe@localhost.localdomain>
2013-07-10 15:06:09 -0400

    lstmux now highlights what session you are in

--- bashincludes/lstmux.sh
+++ bashincludes/lstmux.sh
@@ -2,16 +2,26 @@
 
 function lstmux {
 	SESSIONS=$(tmux list-sessions 2> /dev/null)
+
+	if [[ "$TMUX" ]]
+	then
+	    CURRENT_SESSION=$(tmux display-message -p '#S')
+	fi
+
 	if [[ "$SESSIONS" ]] 
 	then
 	     
 		echo -en "$COLOR_BGreen"
 		echo "Available Tmux Sessions: "
-		echo -en "$COLOR_Green"
-		
-		echo "$SESSIONS" | sed 's/^/    /'
-
 		echo -en "$COLOR_off"
+
+		echo -en "$COLOR_Green"
+		echo "$SESSIONS" | 
+		    ack --passthru --color --color-match=green ".*$CURRENT_SESSION:.*" |
+		    sed 's/^/    /'
+		
+		echo -en "$COLOR_off"
+
 	fi
 }
 
