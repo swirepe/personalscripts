9d675c5bcc03841da4dc8b8568b101d579f058e5
Peter Swire <swirepe@gmail.com>
2013-07-13 01:00:07 -0400

    removed some fluff from include.sh

--- bashincludes/INCLUDE.sh
+++ bashincludes/INCLUDE.sh
@@ -61,3 +61,4 @@ source_include $BASHINCLUDES_DIR/ramdiskscripts.sh
 source_include $BASHINCLUDES_DIR/virtualenvs.sh
 source_include $BASHINCLUDES_DIR/errorinred.sh
 source_include $BASHINCLUDES_DIR/lstmux.sh
+source_include $BASHINCLUDES_DIR/lslan.sh
