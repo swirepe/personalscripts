cbec14dc8ae50c3e2507bf11333701a4f4d13445
Peter Swire <swirepe@gmail.com>
2013-07-18 22:58:53 -0400

    show the vagrant box in the prompt

--- bashincludes/vagrant.sh
+++ bashincludes/vagrant.sh
@@ -1,1 +1,13 @@
 PATH="$PATH:/opt/vagrant/bin"
+
+# display the vagrant box in the prompt when we are in a vm
+function _getvagrantbox {
+    grep 'config\.vm\.box\s*=' Vagrantfile | sed 's/.*"\(.*\)".*/\1/'
+    
+}
+
+
+if [ -e /vagrant/Vagrantfile ]
+then
+    export PS1="${COLOR_Purple}[vm:$(_getvagrantbox)]$COLOR_off $PS1"
+fi
