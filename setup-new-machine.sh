9dba18af16d1b82bdee7b425133662a42ac258b1
Peter Swire <swirepe@gmail.com>
2013-08-02 04:09:37 -0400

    added a bit for fetching gnu parallel

--- setup-new-machine.sh
+++ setup-new-machine.sh
@@ -185,6 +185,17 @@ then
     sudo make install
     echo -e "${COLOR_BGreen}Build of git-extras complete.${COLOR_off}"
     
+    
+    echo -e "${COLOR_Blue}Fetching and installing gnu parallel.${COLOR_off}"
+    cd /tmp
+    wget http://ftp.gnu.org/gnu/parallel/parallel-latest.tar.bz2
+    tar -xvjf parallel-latest.tar.bz2
+    cd parallel-latest
+    ./configure
+    make
+    sudo make install
+    echo -e "${COLOR_BGreen}Install of gnu parallel complete.${COLOR_off}"
+    
 fi
 
 
