ebc553a97f44be91188d19e8fe3f40ad3b177e43
Peter Swire <swirepe@localhost.localdomain>
2013-08-08 12:56:50 -0400

    changed pcat to try to guess some lexers from shebangs.  viewsh picked up this change

--- bashincludes/pyg.sh
+++ bashincludes/pyg.sh
@@ -3,8 +3,33 @@
 # http://www.ralfebert.de/blog/tools/cat_syntax_highlighting/
 
 function pcat {
-   # redirect to cat if we can't find a lexer
-   pygmentize $@ 2> /dev/null || cat $@
+    FILE="$1"
+    LEXER=""
+    FIRSTLINE="$(head -n1 $FILE)"
+
+    if [[ "$FIRSTLINE" == *"bash"* ]]
+    then
+	LEXER="-l bash"
+    fi
+
+    if [[ "$FIRSTLINE" == *"python"* ]]
+    then
+	LEXER="-l python"
+    fi
+
+    if [[ "$FIRSTLINE" == *"perl"* ]]
+    then
+	LEXER="-l perl"
+    fi
+
+    if [[ "$FIRSTLINE" == *"ruby"* ]]
+    then
+	LEXER="-l ruby"
+    fi
+
+
+    pygmentize $LEXER "$FILE" 2> /dev/null || cat "$FILE"
+
 }
 
 function pless() {
