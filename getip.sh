ea8475b4c18bf09dbbc0cf68b3d164fdc9b6d33f
Peter Swire <swirepe@gmail.com>
2013-08-18 23:13:25 -0400

    now prints local ips too

--- getip.sh
+++ getip.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env bash
+# http://www.unix.com/shell-programming-scripting/159411-parsing-ifconfig-output.html
+#ifconfig -a | awk '$2~/^Link/{_1=$1;getline;if($2~/^addr/){print _1" "$2}}'
+
+if [ `uname | awk '{print $1}'` == "Linux" ]
+then
+    /sbin/ifconfig -a |
+    awk '$2~/^Link/{_1=$1;getline;if($2~/^addr/){print _1" "$2}}'
+else 
+    ifconfig -a |
+    awk '$2~/^flags/{_1=$1;getline;if($1~/^inet/){print _1" "$2}}'
+fi
+
 echo "ifconfig.me: $(curl ifconfig.me)"
 echo "ipogre:      $(curl ipv4.ipogre.com)"
