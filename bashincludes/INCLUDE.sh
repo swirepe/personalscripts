7c020d3816368adc02444c9832e70ee4d3e232c9
Peter Swire <swirepe@gmail.com>
2013-09-01 02:11:14 -0400

    aliases for viewing iptables stuff

--- bashincludes/INCLUDE.sh
+++ bashincludes/INCLUDE.sh
@@ -72,4 +72,5 @@ source_include $BASHINCLUDES_DIR/errorinred.sh
 source_include $BASHINCLUDES_DIR/lstmux.sh
 source_include $BASHINCLUDES_DIR/lsfn.sh
 source_include $BASHINCLUDES_DIR/z.sh
+source_include $BASHINCLUDES_DIR/iptables.sh
 source_include $BASHINCLUDES_DIR/mac.sh
---
+++ bashincludes/iptables.sh
@@ -0,0 +1,12 @@
+# let me list stuff from iptables
+# http://www.cyberciti.biz/tips/bash-aliases-mac-centos-linux-unix.html
+
+## shortcut  for iptables and pass it via sudo#
+alias ipt='sudo /sbin/iptables'
+ 
+# display all rules #
+alias iptlist='sudo /sbin/iptables -L -n -v --line-numbers'
+alias iptlistin='sudo /sbin/iptables -L INPUT -n -v --line-numbers'
+alias iptlistout='sudo /sbin/iptables -L OUTPUT -n -v --line-numbers'
+alias iptlistfw='sudo /sbin/iptables -L FORWARD -n -v --line-numbers'
+alias firewall=iptlist
