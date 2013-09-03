30c91ebc618566f84420d4eb8eff6e5225670db2
Peter Swire <swirepe@gmail.com>
2013-09-03 10:33:39 -0400

    darwin uses .dylib instead of .so so stderred changed to reflect that

--- bashincludes/errorinred.sh
+++ bashincludes/errorinred.sh
@@ -3,7 +3,8 @@
 
 
 # see https://github.com/sickill/stderred
-if [[ -f "$SCRIPTS_DIR/src/stderred/build/libstderred.so" ]]
+if [[ -f "$SCRIPTS_DIR/src/stderred/build/libstderred.so" ]] || [[ -f "$SCRIPTS_DIR/src/stderred/build/libstderred.dylib" ]]
+
 then
     # preloads are different on different machines
     case "$OSTYPE" in
@@ -11,7 +12,7 @@ then
 	    export LD_PRELOAD="$SCRIPTS_DIR/src/stderred/build/libstderred.so${LD_PRELOAD:+:$LD_PRELOAD}"
 	    ;;
 	darwin*)
-	   export DYLD_INSERT_LIBRARIES="$SCRIPTS_DIR/src/stderred/build/libstderred.so${LD_PRELOAD:+:$LD_PRELOAD}"
+	   export DYLD_INSERT_LIBRARIES="$SCRIPTS_DIR/src/stderred/build/libstderred.dylib${LD_PRELOAD:+:$LD_PRELOAD}"
 	    ;;
 	*)   
 	    [ ! -f ~/.hushlogin ] && echo "[errorinred.sh] platform $OSTYPE not found."
