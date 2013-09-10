9f65df3b8cd7de107c9bd48e241c9389ebcd8d1f
Peter Swire <swirepe@gmail.com>
2013-09-10 15:43:28 -0400

    i guess i can add interfaces in a loop, so

--- pi/add-loopback.sh
+++ pi/add-loopback.sh
@@ -2,3 +2,9 @@
 # this program just makes 127.0.0.2 part of the loopback too
 # use it for distractingsites
 sudo ifconfig lo0 alias 127.0.0.2 up
+
+for i in {2..254}
+do
+    sudo ifconfig lo0 alias 127.0.0.$i up
+    
+done
