77013c35f9877c69667253a711fc9872b1915bd6
Peter Swire <swirepe@gmail.com>
2013-09-10 22:15:57 -0400

    added an alias for the airport utility hidden deep in the mac bowels

--- bashincludes/mac.sh
+++ bashincludes/mac.sh
@@ -1,6 +1,9 @@
 
-if [ "$(uname)" == "Darwin" ]
+if [[ "$(uname)" == "Darwin" ]]
 then
     ## for allowing files to be run that have been downloaded
     alias allow="xattr -d com.apple.quarantine"
+
+    alias airport="/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport"
+
 fi
