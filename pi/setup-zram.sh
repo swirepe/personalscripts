a8da18dd2aa8cdbc35087c0713ca63b428f982ad
Peter Swire <swirepe@gmail.com>
2013-09-11 23:35:48 -0400

    added a thing for setting up zram

---
+++ pi/setup-zram.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# http://gionn.net/2012/03/11/zram-on-debian-ubuntu-for-memory-overcommitment/
+# http://www.reddit.com/r/sysadmin/comments/15kmby/using_compressed_ram_for_memoryovercommit_on_linux/
+
+echo "Installing zram"
+sudo wget https://raw.github.com/gionn/etc/master/init.d/zram -O /etc/init.d/zram
+sudo chmod +x /etc/init.d/zram
+sudo update-rc.d zram defaults
+	
+echo "Starting zram"
+sudo /etc/init.d/zram start
+
+dmesg | grep zram
+cat /proc/swaps
+
