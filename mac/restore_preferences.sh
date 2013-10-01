4b69b9e745a4264bc8a55d3b42e44281fa5de4d6
Peter Swire <swirepe@gmail.com>
2013-10-01 12:01:35 -0400

    right location for preferences now

--- mac/restore_preferences.sh
+++ mac/restore_preferences.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 mv ~/Library/Preferences ~/Library/Preferences-$(date +"%Y-%m-%d")
 mkdir ~/Library/Preferences
-tar xvf ~/pers/scripts/preferences.tar.gz -C ~/Library/Preferences
+tar xvf ~/pers/scripts/mac/preferences.tar.gz -C ~/Library/Preferences
 
