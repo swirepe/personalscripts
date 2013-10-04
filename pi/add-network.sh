93f2d1494bc59d159ef74cacbdbd2ded5b276059
Peter Swire <swirepe@gmail.com>
2013-10-04 10:27:15 -0400

    remote hosts and a shortcut to getting to tripmonster

--- pi/add-network.sh
+++ pi/add-network.sh
@@ -10,7 +10,15 @@ function add_host {
     fi
 }
 
+
 function ssh_host {
+    plain_ssh_host $@
+    remote_ssh_host $@
+    
+}
+
+
+function plain_ssh_host {
     H="$1"
     U="$2"
     if [[ -z "$U" ]]
@@ -26,6 +34,29 @@ function ssh_host {
         echo -e "\n"              | tee --append ~/.ssh/config
     fi
 }
+
+
+function remote_ssh_host {
+    H="$1"
+    U="$2"
+    if [[ -z "$U" ]]
+    then
+        U="swirepe"
+    fi
+    if [[ -z "$(grep r$H ~/.ssh/config)" ]]
+    then
+        echo "Host r$H"            | tee --append ~/.ssh/config
+        echo "    User $U"         | tee --append ~/.ssh/config
+        echo "    Hostname $H"     | tee --append ~/.ssh/config
+        echo "    Port 212"        | tee --append ~/.ssh/config
+        echo "    ProxyCommand  ssh -p 212 swirepe@neuroky.me nc %h %p 2> /dev/null" | tee --append ~/.ssh/config
+        echo -e "\n"               | tee --append ~/.ssh/config
+    fi    
+    
+    
+}
+
+
 
 echo -e "\n\n## ---------------------------------------" | tee --append ~/.ssh/config
 echo -e "## added on $(date) by add-network.sh"          | tee --append ~/.ssh/config
