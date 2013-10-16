298400d2004547b6f0cca4b91a463d8305c91615
Peter Swire <swirepe@gmail.com>
2013-10-16 14:51:54 -0400

    install something too

--- pi/add-remote-drive.sh
+++ pi/add-remote-drive.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+sagi rpcbind nfs-common
 sudo mkdir -p /media/remote
 echo "## created on $(date) by add-remote-drive.sh" | sudo tee --append /etc/fstab
 echo "ray:/media/mass    /media/remote   nfs    rsize=8192,wsize=8192,timeo=14,intr" | sudo tee --append /etc/fstab
