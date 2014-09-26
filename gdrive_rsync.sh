f51b61fb3ac383914279b1c0756c8f079aac855b
Peter Swire <swirepe@gmail.com>
2014-09-26 02:12:06 -0400

    changed the logic for remounting google drive

--- gdrive_rsync.sh
+++ gdrive_rsync.sh
@@ -11,6 +11,10 @@ trap "echo Exited! > /dev/stderr ; exit 1;" SIGINT SIGTERM
 MAX_RETRIES=5000
 i=0
 
+echo "Mounting google drive."
+google-drive-ocamlfuse /home/swirepe/gdrive || $(echo "Failed to mount /home/swirepe/gdrive" > /dev/stderr && exit 1)
+
+
 echo "Attempting rsync $*" >/dev/stderr
 echo "Maximum attempts: $MAX_RETRIES" >/dev/stderr
 
@@ -19,17 +23,21 @@ false
  
 while [ $? -ne 0 -a $i -lt $MAX_RETRIES ]
 do
-    i=$(($i+1))
-    echo "Attempt $i" > /dev/stderr
+	i=$(($i+1))
+	echo "Attempt $i" > /dev/stderr
+	rsync --partial --archive --progress --verbose --inplace ~/sftp ~/gdrive
+	if [ $? -ne 0 ]
+	then
+		echo "Remounting." > /dev/stderr
 		sudo umount /home/swirepe/gdrive &&
-		google-drive-ocamlfuse /home/swirepe/gdrive &&
-    rsync --partial --archive --progress --verbose --inplace ~/sftp ~/gdrive
+		google-drive-ocamlfuse /home/swirepe/gdrive 
+	fi
 done
  
 if [ $i -eq $MAX_RETRIES ]
 then
-    echo "Hit maximum number of retries, giving up." > /dev/stderr
-    exit 1
+	echo "Hit maximum number of retries, giving up." > /dev/stderr
+	exit 1
 fi
 
 exit 0
