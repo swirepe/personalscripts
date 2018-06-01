4246bf7686f67ba2e566e221d290deef42d6d323
Peter Swire <swirepe@swirepe.com>
2018-06-01 19:49:17 -0400

    set a default gopath

--- bashincludes/INCLUDE.sh
+++ bashincludes/INCLUDE.sh
@@ -82,3 +82,4 @@ source_include $BASHINCLUDES_DIR/bt.sh
 source_include $BASHINCLUDES_DIR/psql.sh
 #source_include $BASHINCLUDES_DIR/eclipse.sh
 source_include $BASHINCLUDES_DIR/tiny-care-terminal.sh
+source_include $BASHINCLUDES_DIR/gopath.sh
